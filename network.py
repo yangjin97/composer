628c307b2e0b6d123fcd42b41606ab885ae33ba5
yangjin97 <yangjin@andrew.cmu.edu>
2018-05-21 20:45:19 -0700

    train init

--- network.py
+++ network.py
@@ -4,15 +4,26 @@ class LSTM():
   def __init__(self, time_steps, num_features):
     self.time_steps = time_steps
     self.num_features = num_features
+    self.optimizer = tf.train.RMSPropOptimizer(0.9)
     
 
   def build_network(self):
-    self.frames = tf.placeholder(tf.float32, [time_steps, num_features])
+    with tf.variable_scope("global"):
+      self.frames = tf.placeholder(tf.float32, [self.time_steps, self.num_features])
+      self.lstm = tf.contrib.rnn.BasicLSTMCell(self.num_features)
 
-    hidden_state = tf.zeros([1, lstm.state_size])
-		current_state = tf.zeros([1, lstm.state_size])
+      hidden_state = tf.zeros([1, lstm.state_size])
+      current_state = tf.zeros([1, lstm.state_size])
 
-    state = hidden_state, current_state
+      state = hidden_state, current_state
 
-    for frame in self.frames:
-      
+      self.loss = 0.0
+      for i in range(len(self.frames)-1):
+        output, state = self.lstm(self.frames[i], state)
+
+        self.loss += tf.losses.mean_squared_error(self.frames[i+1])
+
+      tvars = tf.trainable_variables(scope="global")
+      grads = tf.gradients(self.loss, tvars)
+
+      self.train_op = self.optimizer.apply_gradients(zip(grads, tvars))
