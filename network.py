5234ccfa6403593754b03882eac4517cb46a6186
yangjin97 <yangjin@andrew.cmu.edu>
2018-06-09 17:46:01 -0700

    compile with predict

--- network.py
+++ network.py
@@ -5,23 +5,35 @@ class LSTM():
     self.time_steps = time_steps
     self.num_features = num_features
     self.optimizer = tf.train.RMSPropOptimizer(0.9)
-    
+    self.build_network()
+    self.build_loss()
 
   def build_network(self):
     with tf.variable_scope("global"):
-      self.frames = tf.placeholder(tf.float32, [self.time_steps, self.num_features])
+      self.input = tf.placeholder(tf.float32, [1, self.num_features])
       self.lstm = tf.contrib.rnn.BasicLSTMCell(self.num_features)
 
-      hidden_state = tf.zeros([1, lstm.state_size])
-      current_state = tf.zeros([1, lstm.state_size])
+      hidden_state = tf.zeros([1, self.num_features])
+      current_state = tf.zeros([1, self.num_features])
+      self.init_state = hidden_state, current_state
 
-      state = hidden_state, current_state
+      state = self.init_state
+      self.outputs = []
+      for i in range(self.time_steps):
+        output, state = self.lstm(self.input, state)
+        self.outputs.append(output)
 
+  def build_loss(self):
+    with tf.variable_scope("global"):
+      self.frames = tf.placeholder(tf.float32, [self.time_steps, self.num_features])
+
+      state = self.init_state
       self.loss = 0.0
-      for i in range(len(self.frames)-1):
-        output, state = self.lstm(self.frames[i], state)
+      for i in range(self.frames.shape[0]-1):
+        output, state = self.lstm(self.frames[i][None,:], state)
 
-        self.loss += tf.losses.mean_squared_error(self.frames[i+1])
+        self.loss += tf.losses.mean_squared_error(self.frames[i+1][None,:],
+            output)
 
       tvars = tf.trainable_variables(scope="global")
       grads = tf.gradients(self.loss, tvars)
--- train.py
+++ train.py
@@ -1,4 +1,48 @@
 from parser import Parser
 from network import LSTM
+import tensorflow as tf
+import numpy as np
 
+class Runner():
+  def __init__(self):
+    self.parser = Parser('bach/chorales/01ausmei.mid')
+    self.frames, self.num_tracks, self.min_time = self.parser.parse()
+    self.time_steps = len(self.frames)
+    self.num_features = len(self.frames[0])
+    self.network = LSTM(self.time_steps, self.num_features)
 
+  def train(self):
+    feed_dict = {
+        self.network.frames: self.frames
+        }
+
+    with tf.Session() as sess:
+      sess.run(tf.global_variables_initializer())
+      loss, _ = sess.run([self.network.loss, self.network.train_op], \
+          feed_dict=feed_dict)
+    # return loss
+
+  def predict(self):
+    feed_dict = {
+        self.network.input: self.frames[0][None,:]
+        }
+
+    with tf.Session() as sess:
+      sess.run(tf.global_variables_initializer())
+      outputs = sess.run(self.network.outputs, feed_dict=feed_dict)
+
+    get_note = lambda x : 1 if x > 0.5 else 0
+    vget_note = np.vectorize(get_note)
+    for i in range(len(outputs)):
+      outputs[i] = vget_note(outputs[i].flatten())
+    
+    return outputs
+
+def main():
+  runner = Runner()
+  runner.train()
+  outputs = runner.predict()
+  runner.parser.make_midi(outputs, runner.num_tracks, 64, runner.min_time)
+
+if __name__ == "__main__":
+  main()
